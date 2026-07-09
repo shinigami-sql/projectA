3ee33999dfb86e5a9ca6e5db71667ac96b88117e
shinigami-sql <erngomezmatos@gmail.com>
2026-07-09 10:09:20 -0400

    Add main.py - program entry point

---
+++ main.py
@@ -0,0 +1,20 @@
+print('\n')
+print('''Welcome to the pL-Otter!
+
+Here, we can build all figures you can think of, the weirdest, most magnifique, out of the world figures.
+
+All you need to do is answer a few questions below:
+''')
+
+choice = input("Do you want to build your own or load a list? ")
+
+if choice == "list":
+    # TODO: Ask user for file type (CSV or JSON)
+    # TODO: Show user the required format for each file type
+    # TODO: Validate file matches expected format
+    # TODO: If format is wrong, ask if they want to build their own instead
+    pass
+
+elif choice == "build":
+    # TODO: Build new figure flow
+    pass
