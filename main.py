b2fe26a5bc0b211a0f8a8e094d6538694cfd94d6
shinigami-sql <erngomezmatos@gmail.com>
2026-07-13 21:29:54 -0400

    Fixed indent error

--- main.py
+++ main.py
@@ -10,19 +10,19 @@ print(list_of_figures, '\n')
 choice = input("")
 
 if choice == "Triangle":
-        print('Three corners? Not bad...')
-    elif choice == 'Square':
-        print('Four corners? Look at ya!')
-    elif choice == 'Pentagon':
-        print('Secretary of Defense? Roger that!')
-    elif choice == 'Hexagon':
-        print('Honeycombs? Sweet!')
-    elif choice == 'Heptagon':
-        print('Mathematical?')
-    elif choice == 'Octagon':
-        print('You want to start a fight?')
-    else:
-        print('Match must the figure name, or error you will get my young Padawan')
+    print('Three corners? Not bad...')
+elif choice == 'Square':
+    print('Four corners? Look at ya!')
+elif choice == 'Pentagon':
+    print('Secretary of Defense? Roger that!')
+elif choice == 'Hexagon':
+    print('Honeycombs? Sweet!')
+elif choice == 'Heptagon':
+    print('Mathematical?')
+elif choice == 'Octagon':
+    print('You want to start a fight?')
+else:
+    print('Match must the figure name, or error you will get my young Padawan')
 
 
 
