a30ad0218397bf3cd8a1755f53823660d13e1dae
shinigami-sql <erngomezmatos@gmail.com>
2026-09-18 14:07:09 -0400

    Missed saving SQL statement before moving it to data_pipeline folder

--- sql_models/silver_plots_enriched.sql
+++ sql_models/silver_plots_enriched.sql
@@ -1,1 +1,16 @@
-INSERT INTO silver.plots_enriched
+INSERT INTO silver.plots_enriched (plot_turn, figure_title, is_triangle, triangle_type_title, dimension_title, rotation, date, time, day_of_week, is_weekday)
+SELECT plot_turn as plot_turn,
+INITCAP(figure) as figure_title,
+CASE WHEN INITCAP(figure) = 'Triangle' THEN TRUE else FALSE END AS is_triangle,
+CASE WHEN triangle_type = 'e' THEN 'Equilateral'
+WHEN triangle_type = 'i' THEN 'Isoceles'
+WHEN triangle_type = 'r' THEN 'Right Triangle' END AS triangle_type_title,
+CASE WHEN dimension = '2d' THEN '2D'
+WHEN dimension = '3d' THEN '3D' END AS dimension_title,
+rotation as rotation,
+CAST(date as DATE) as date,
+date::TIME as time,
+TO_CHAR(date, 'Day') as day_of_week,
+CASE WHEN TO_CHAR(date, 'D') IN ('1', '7') THEN FALSE ELSE TRUE END AS is_weekday
+FROM raw.plots
+WHERE id NOT IN (SELECT id FROM silver.plots_enriched)
